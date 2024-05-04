b69029dd12bfb24cb783f8f620e3d32a18476344
Leftfield1 <62058651+Leftfield1@users.noreply.github.com>
2024-05-04 11:17:27 +0200

    Update SeoTextWidth.js

--- SeoTextWidth.js
+++ SeoTextWidth.js
@@ -1,32 +1,47 @@
 $(document).ready(function() {
-    
-    // Source: https://www.screamingfrog.co.uk/page-title-meta-description-lengths-by-pixel-width/
-    // 16px Arial
+
     // 512 px for Meta Title - desktop - Inputfield_seo
     // 920 px of Meta Description - desktop - Inputfield_desc
     // Inputfield_seo - this is what you have to change to your own field, so find the ID of the field and change it to Inputfield_seo_title or whatever.
     // Inputfield_desc - this is what you have to change to your own field, so find the ID of the field and change it to Inputfield_seo_description or whatever.
 
     // Function to calculate text width
-    function calculateTextWidth(text) {
-      const canvas = calculateTextWidth.canvas || (calculateTextWidth.canvas = document.createElement("canvas"));
-      const context = canvas.getContext("2d");
+    function calculateTextTitleWidth(text) {
+        const canvas = calculateTextTitleWidth.canvas || (calculateTextTitleWidth.canvas = document.createElement("canvas"));
+        const context = canvas.getContext("2d");
+        // Everyone (Google too) is calculating width 13px.
+        const fontWeight = 'normal';
+        const fontSize = '18px';
+        const fontFamily = 'arial, sans-serif';
 
-      const fontWeight = 'normal';
-      const fontSize = '16px';
-      const fontFamily = 'arial, sans-serif';
+        context.font = `${fontWeight} ${fontSize} ${fontFamily}`;
+        const metrics = context.measureText(text);
+        // resolving browser issues
+        const textWidth = metrics.actualBoundingBoxRight + metrics.actualBoundingBoxLeft;
 
-      context.font = `${fontWeight} ${fontSize} ${fontFamily}`;
-      const metrics = context.measureText(text);
-      const textWidth = metrics.actualBoundingBoxRight + metrics.actualBoundingBoxLeft;
-      return textWidth;
+        return textWidth;
         
     }
-    
+    function calculateTextDescriptionWidth(text) {
+        const canvas = calculateTextDescriptionWidth.canvas || (calculateTextDescriptionWidth.canvas = document.createElement("canvas"));
+        const context = canvas.getContext("2d");
+        // Everyone (Google too) is calculating width 13px.
+        const fontWeight = 'normal';
+        const fontSize = '13px';
+        const fontFamily = 'arial, sans-serif';
+
+        context.font = `${fontWeight} ${fontSize} ${fontFamily}`;
+        const metrics = context.measureText(text);
+        // resolving browser issues
+        const textWidth = metrics.actualBoundingBoxRight + metrics.actualBoundingBoxLeft;
+
+        return textWidth;
+        
+    } 
 
     // Function to check and apply class based on width
-    function checkTextLength(text, maxLength, fieldName, spanElement) {
-        const width = calculateTextWidth(text);
+    function checkTextTitleLength(text, maxLength, fieldName, spanElement) {
+        const width = calculateTextTitleWidth(text);
         if (width > maxLength) {
             const warningIcon = '<i class="fa fa-exclamation-circle"></i>'; // Font Awesome warning icon
             spanElement.html(`${warningIcon} <span class="text-danger">Warning:</span> Text in field '${fieldName}' exceeds maximum width (${maxLength}px).`);
@@ -36,14 +51,25 @@ $(document).ready(function() {
             spanElement.removeClass('text-danger');
         }
     }
-
+    // Function to check and apply class based on width
+    function checkTextDescriptionLength(text, maxLength, fieldName, spanElement) {
+        const width = calculateTextDescriptionWidth(text);
+        if (width > maxLength) {
+            const warningIcon = '<i class="fa fa-exclamation-circle"></i>'; // Font Awesome warning icon
+            spanElement.html(`${warningIcon} <span class="text-danger">Warning:</span> Text in field '${fieldName}' exceeds maximum width (${maxLength}px).`);
+            spanElement.addClass('text-danger');
+        } else {
+            spanElement.text(`Estimated Width: ${width}px`);
+            spanElement.removeClass('text-danger');
+        }
+    }
     // Handle Meta Title seo field
     // Change ID Inputfield_seo into Inputfield_YourFieldName
     const seoInput = $('#Inputfield_seo');
     const seoSpan = $('<span class="detail" id="seo-width"></span>');
     seoInput.keyup(function() {
     const seoText = $(this).val();
-    checkTextLength(seoText, 512, 'Inputfield_seo', seoSpan);
+    checkTextTitleLength(seoText, 512, 'Inputfield_seo', seoSpan);
     seoInput.after(seoSpan); // the span after the input
     });
 
@@ -53,7 +79,7 @@ $(document).ready(function() {
     const descSpan = $('<span class="detail" id="desc-width"></span>');
     descInput.keyup(function() {
     const descText = $(this).val();
-    checkTextLength(descText, 920, 'Inputfield_desc', descSpan);
+    checkTextDescriptionLength(descText, 920, 'Inputfield_desc', descSpan);
     descInput.after(descSpan); // the span after the input
     });
 });
